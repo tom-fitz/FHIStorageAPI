71a7522d67b3a881e38b2bcc360761cd3adc8a46
Thomas Fitzgerald <tpfitz42@gmail.com>
2019-05-22 16:04:55 -0500

    Update Startup.cs

--- FHIStorage.API/FHIStorage.API/Startup.cs
+++ FHIStorage.API/FHIStorage.API/Startup.cs
@@ -44,7 +44,7 @@ namespace FHIStorage.API
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, HouseInfoContext houseInfoContext)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
             {
